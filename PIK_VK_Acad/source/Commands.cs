a4403d7018812ed312b86b14e5b9d416a04c5e49
Vildar Khisyametdinov <vildar82@gmail.com>
2016-07-21 19:37:12 +0300

    VK_SetSpecCellSubheadStyle

---
+++ PIK_VK_Acad/source/Commands.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.Runtime;
+
+namespace PIK_VK_Acad
+{
+    public class Commands
+    {
+        const string group = AcadLib.Commands.Group;
+
+        /// <summary>
+        /// Установка подзаголовочного стиля выбранной ячейке таблицы
+        /// </summary>
+        [CommandMethod(group, nameof(VK_SetSpecCellSubheadStyle), CommandFlags.Modal)]
+        public void VK_SetSpecCellSubheadStyle()
+        {
+            AcadLib.CommandStart.Start(doc =>
+            {
+                Spec.TableService ts = new Spec.TableService(doc);
+                ts.SetCellSubheadStyle();
+            });
+        }
+    }
+}
